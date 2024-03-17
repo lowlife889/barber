5c2cec675f0550f3edb9e38021d535e8b11bb473
bruno <brunominighin@gmail.com>
2024-03-17 05:49:18 -0300

    sixth commit

--- src/libs/firebase.js
+++ src/libs/firebase.js
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app";
 import "firebase/auth"
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBe9GypczjOmkeJOGb2RA0uZaZ9C2a3JAA",
-  authDomain: "barber-d4311.firebaseapp.com",
-  projectId: "barber-d4311",
-  storageBucket: "barber-d4311.appspot.com",
-  messagingSenderId: "249558987001",
-  appId: "1:249558987001:web:21e22db1e0ad7731703228",
-  measurementId: "G-H04GYKPJSF"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase
